30bee0b194191f67531272e5b04592a72b0e3019
semeeenov_viktor <vsemenov@asdgroup.pro>
2019-11-21 13:35:50 +0700

    remove language detector

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
         .package(url: "https://github.com/hallee/vapor-simple-file-logger.git", from: "1.0.1"),
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0"),
-        .package(url: "https://github.com/d-a-n/swift-language-detector", from: "1.0.1"),
         .package(url: "https://github.com/Moya/Moya.git", from: "13.0.0"),
     ],
     targets: [
